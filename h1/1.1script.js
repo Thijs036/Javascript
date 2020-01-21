51ee267e978d0d1d4b88bdffe8928bc05d173f4d
Thijs <t.oldenburger@hotmail.nl>
2020-01-21 14:06:53 +0100

    Initial commit

---
+++ h1/1.1script.js
@@ -0,0 +1,1 @@
+console.log("Hallo Thijs, Hoe gaat het ermee?")
---
+++ h2/2.1script.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("paginatitel").innerText =("Dit is de Paginatitel");
+document.getElementById("subkop").innerText =("Dit is de Subkop");
+document.getElementById("aap1").src = "h2/img/ihattaren.jpg";
+document.getElementById("aap2").src = "h2/img/malen.jpg";
